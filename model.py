ab41138eb20e9062e6fda6997850080aec38dca8
Clay <41354715+ccs96307@users.noreply.github.com>
2020-01-09 22:54:45 +0800

    Initial Upload

---
+++ model.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import torch.nn as nn
+
+
+class discriminator(nn.Module):
+    def __init__(self):
+        super(discriminator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(784, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class generator(nn.Module):
+    def __init__(self):
+        super(generator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(128, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        return self.main(input)
---
+++ test.py
@@ -0,0 +1,37 @@
+import torch
+from torchvision import transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+def show_images(images):
+    sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
+
+    for index, image in enumerate(images):
+        plt.subplot(sqrtn, sqrtn, index+1)
+        plt.imshow(image.reshape(28, 28))
+
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+print('GPU State:', device)
+
+# Model
+G = torch.load('Generator_epoch_200.pth')
+G.eval()
+
+# Generator
+noise = (torch.rand(16, 128)-0.5) / 0.5
+noise = noise.to(device)
+
+fake_image = G(noise)
+imgs_numpy = (fake_image.data.cpu().numpy()+1.0)/2.0
+show_images(imgs_numpy)
+plt.show()
+
